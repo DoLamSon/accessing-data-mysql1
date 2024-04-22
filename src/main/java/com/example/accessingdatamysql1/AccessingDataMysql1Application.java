b731d1ae5a250e638b68aa6df31676cd7f677d8a
sondl <sondl@bsscommerce.com>
2024-04-22 16:32:09 +0700

    first commit

---
+++ src/main/java/com/example/accessingdatamysql1/AccessingDataMysql1Application.java
@@ -0,0 +1,14 @@
+package com.example.accessingdatamysql1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataMysql1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AccessingDataMysql1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/accessingdatamysql1/AccessingDataMysql1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatamysql1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataMysql1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
